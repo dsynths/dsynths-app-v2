a997a46f0a3c607fd97804ec6a3c40737d31af7b
coinlordd <35095979+coinlordd@users.noreply.github.com>
2022-02-10 15:25:05 +0100

    DEUS shoutout

--- src/pages/trade.tsx
+++ src/pages/trade.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import Image from 'next/image'
 import { isMobile } from 'react-device-detect'
 
 import { Trade as TradeComponent, LineChart } from 'components/App/Trade'
+import DeusLogo from '/public/static/images/tokens/deus.svg'
+import { ExternalLink } from 'components/Link'
 
 const Container = styled.div`
   display: flex;
@@ -18,11 +21,27 @@ const Container = styled.div`
   `}
 `
 
+const DeusBanner = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  font-size: 13px;
+  color: ${({ theme }) => theme.text2};
+  gap: 5px;
+  margin-top: 24px;
+  opacity: 0.5;
+`
+
 export default function Trade() {
   return (
     <Container>
       {!isMobile && <LineChart />}
       <TradeComponent />
+      <DeusBanner>
+        <Image src={DeusLogo} alt="DEUS Logo" width="15px" height="15px" />
+        <ExternalLink href="https://deus.finance">Powered by DEUS Finance</ExternalLink>
+      </DeusBanner>
     </Container>
   )
 }
